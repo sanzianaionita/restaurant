15e1f07baf47903f0a0ab3f51af256735c5d1b4c
Raisa1224 <58228764+Raisa1224@users.noreply.github.com>
2022-12-03 18:56:54 +0200

    Added new tests and corrected some old ones

---
+++ app/src/test/java/com/example/restaurant18/LoginTest.java
@@ -0,0 +1,54 @@
+package com.example.restaurant18;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.example.restaurant18.DAO.UserDAO;
+import com.example.restaurant18.entity.User;
+import com.example.restaurant18.ui.login.LoginFragment;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class LoginTest {
+    private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private  static final String URL = "jdbc:oracle:thin:@192.168.100.34:1521:XE";
+    private static final String USERNAME = "raisa";
+    private static final String PASSWORD = "Sasakisan";
+
+    private Connection connection;
+
+    private void createConnection(){
+        try {
+            Class.forName(DRIVER);
+            this.connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void retrieveUserByEmailAndPasswordTest() throws SQLException {
+        createConnection();
+
+        UserDAO userDAO = new UserDAO(connection);
+        User userExtracted = userDAO.getUserByCredentials("admin@mail.com", "admin");
+
+        User correctUser = new User(0, "admin", "admin", "admin@mail.com",
+                "admin","mrs","24-12-2000");
+
+        assertEquals(userExtracted, correctUser);
+    }
+    
+
+}
